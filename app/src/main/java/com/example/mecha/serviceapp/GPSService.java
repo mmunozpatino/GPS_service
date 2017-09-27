3348b301fff910eae1e8c4654a74606cf305b874
Mercedes <mercedesmp1305@gmail.com>
2017-09-27 11:30:01 -0300

    first commit

---
+++ app/src/main/java/com/example/mecha/serviceapp/GPSService.java
@@ -0,0 +1,81 @@
+package com.example.mecha.serviceapp;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+
+public class GPSService extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+
+    public GPSService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //tomo cada nueva location
+                Intent i = new Intent("location_update");
+                i.putExtra("coordinates", location.getLongitude()+" "+location.getLatitude());
+                sendBroadcast(i);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                //hacemo que active ubicacion si no lo tiene activado
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context
+                .LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, listener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(locationManager != null){
+            locationManager.removeUpdates(listener);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mecha/serviceapp/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.mecha.serviceapp;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.jar.Pack200;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bStart, bStop;
+    private TextView textView;
+    //para permitir que llegue el broadcast
+    private BroadcastReceiver broadcastReceiver;
+
+    //este método permite ir tomando los datos que toma el service
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    textView.append("\n" + intent.getExtras().get("coordinates"));
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bStart = (Button) findViewById(R.id.buttonStart);
+        bStop = (Button) findViewById(R.id.buttonStop);
+        textView = (TextView) findViewById(R.id.textView);
+
+        if(!runtime_permission()){
+            enable_buttons();
+        }
+    }
+
+    private boolean runtime_permission(){
+        if(Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission
+                        .ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission
+                        .ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest
+                    .permission.ACCESS_FINE_LOCATION},100);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void enable_buttons(){
+        bStart.setOnClickListener(this);
+        bStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] ==
+                    PackageManager.PERMISSION_GRANTED){
+                enable_buttons();
+            }else{
+                runtime_permission();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonStart:
+                Intent start = new Intent(getApplicationContext(), GPSService.class);
+                startService(start);
+                break;
+
+            case R.id.buttonStop:
+                Intent stop = new Intent(getApplicationContext(), GPSService.class);
+                stopService(stop);
+                break;
+        }
+    }
+}
